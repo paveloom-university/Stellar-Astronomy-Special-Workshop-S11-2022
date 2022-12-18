84ba42b0cf7421ce5c031c559af9320717200671
Pavel Sobolev <paveloom@riseup.net>
2022-12-18 17:17:44 +0300

    Fit the linear model by numerical optimization.

--- scripts/compute.jl
+++ scripts/compute.jl
@@ -62,7 +62,7 @@ pad = " "^4
 F = Float64
 
 "Integer type used across the script"
-I = UInt64
+I = Int64
 
 # Define the paths
 CURRENT_DIR = @__DIR__
@@ -75,7 +75,9 @@ mkpath(PLOTS_DIR)
 println('\n', " "^4, "> Loading the packages...")
 
 using LaTeXStrings
+using Optim
 using Plots
+using Roots
 
 # Use the PGFPlotsX backend for plots
 pgfplotsx()
@@ -86,13 +88,6 @@ default(
     dpi=300,
     size=(400, 400),
     legend=nothing,
-    linewidth=0.1,
-    markersize=2,
-    markerstrokewidth=0.1,
-    xlabel=L"x",
-    ylabel=L"y",
-    draw_arrow=true,
-    aspect_ratio=:equal,
 )
 
 # Define the data set
@@ -102,6 +97,19 @@ X = [0, 0.9, 1.8, 2.6, 3.3, 4.4, 5.2, 6.1, 6.5, 7.4]
 Y = [5.9, 5.4, 4.4, 4.6, 3.5, 3.7, 2.8, 2.8, 2.4, 1.5]
 ω_Y = [1, 1.8, 4, 8, 20, 20, 70, 70, 100, 500]
 σ_Y = @. 1 / √(ω_Y)
+
+# Compute the size of the data set
+N = length(X)
+
+"Compute a weighted squared difference"
+function diff(x::F, a::F, b::F, i::I)
+    return ω_Y[i] * (a + b * x - Y[i])^2 + ω_X[i] * (x - X[i])^2
+end
+
+"Compute a sum of weighted squared differences"
+function wsum(x::Vector{F}, a::F, b::F)
+    return sum(i -> diff(x[i], a, b, i), 1:N)
+end
 
 """
 Find the best parameters (and their average errors) of
@@ -117,13 +125,8 @@ function york(; b₀::F, ε::F, linear::Bool=false)::Tuple{F,F,F,F,Vector{F},Vec
     b_prev = b₀ + 2ε
     # Keep several global variables for
     # getting the results out of the loop
-    a = 0
-    σ_a = 0
-    σ_b = 0
-    ΔX = similar(X)
-    ΔY = similar(Y)
-    # Compute the size of the data set
-    N = length(X)
+    a = σ_a = σ_b = 0
+    x = y = similar(X)
     # Until the required precision is reached
     while abs(b_prev - b) > ε
         # Save the current `b` as the previous one
@@ -161,40 +164,216 @@ function york(; b₀::F, ε::F, linear::Bool=false)::Tuple{F,F,F,F,Vector{F},Vec
         σ²_a = σ²_b * sum(@. W * X^2) / W_sum
         σ_a = √(σ²_a)
         σ_b = √(σ²_b)
-        # Compute the deviations
-        ΔX = @. -b * W / ω_X * (a + b * X - Y)
-        ΔY = @. W / ω_Y * (a + b * X - Y)
+        # Compute the coordinates of the projections
+        x = @. X + -b * W / ω_X * (a + b * X - Y)
+        y = @. Y + W / ω_Y * (a + b * X - Y)
     end
-    return (a, b, σ_a, σ_b, ΔX, ΔY)
+    return (a, b, σ_a, σ_b, x, y)
 end
 
 println(pad, "> Fitting the linear model using the D. York's method...")
 
 # Fit the linear model
-a_l, b_l, σ_a_l, σ_b_l, ΔX_l, ΔY_l = york(b₀=-0.5, ε=1e-8, linear=true)
-a_c, b_c, σ_a_c, σ_b_c, ΔX_c, ΔY_c = york(b₀=-0.5, ε=1e-8)
+a_l, b_l, σ_a_l, σ_b_l, x_l, y_l = york(b₀=-0.5, ε=1e-8, linear=true)
+a_c, b_c, σ_a_c, σ_b_c, x_c, y_c = york(b₀=-0.5, ε=1e-8)
+
+# Compute the sums of the weighted squared differences
+∑_l = wsum(x_l, a_l, b_l)
+∑_c = wsum(x_c, a_c, b_c)
 
 println('\n', pad, pad, "Linear:")
-println(pad, pad, pad, "a: ", a_l)
-println(pad, pad, pad, "b: ", b_l, '\n')
+println(pad, pad, pad, "a: ", a_l, " ± ", σ_a_l)
+println(pad, pad, pad, "b: ", b_l, " ± ", σ_b_l)
+println(pad, pad, pad, "∑: ", ∑_l, '\n')
 println(pad, pad, "Cubic:")
-println(pad, pad, pad, "a: ", a_c)
-println(pad, pad, pad, "b: ", b_c, '\n')
+println(pad, pad, pad, "a: ", a_c, " ± ", σ_a_c)
+println(pad, pad, pad, "b: ", b_c, " ± ", σ_b_c)
+println(pad, pad, pad, "∑: ", ∑_c, '\n')
 
 println(pad, "> Plotting the results of the D. York's method...")
 
 # Plot the data
-plot(X, Y, xerror=σ_X, yerror=σ_Y)
+scatter(
+    X,
+    Y,
+    xerror=σ_X,
+    yerror=σ_Y,
+    markershape=:xcross,
+    aspect_ratio=:equal,
+    linewidth=0.1,
+    markersize=1,
+    markerstrokewidth=0.1,
+    xlabel=L"x",
+    ylabel=L"y",
+)
 
 # Plot the regression line using the D. York's method
-plot!(x -> a_c + b_c * x)
+plot!(x -> a_c + b_c * x, linewidth=0.1)
 
 # Plot the projections
-for (x, y, δx, δy) in zip(X, Y, ΔX_c, ΔY_c)
-    plot!([x, x + δx], [y, y + δy], arrow=true)
+scatter!(
+    x_c,
+    y_c,
+    markershape=:xcross,
+    markersize=1,
+    markerstrokewidth=0.1,
+)
+for (X, Y, x, y) in zip(X, Y, x_c, y_c)
+    plot!([X, x], [Y, y], linewidth=0.2)
 end
 
 # Save the figure
 savefig(joinpath(PLOTS_DIR, "york.pdf"))
+
+# Define the initial values for the numerical optimization
+θ₀ = [6, -0.5]
+
+"""
+The target function for numerical optimization
+
+It can be shown that this function can be treated as a
+non-constant part of the log likelihood function
+"""
+function target(θ::Vector{F})::F
+    # The input parameters define the line
+    a, b = θ
+    # We go through each point of the input data and
+    # find a point on this line which is the closest
+    return 0.5 * sum(
+        i -> optimize(θ -> diff(θ[1], a, b, i), [X[i]]).minimum,
+        1:N
+    )
+end
+
+"Find the best parameters of the linear model `y = a + bx`
+by numerically minimizing the target function"
+function minimize()::Tuple{F,F,F}
+    res = optimize(target, θ₀)
+    return res.minimizer..., res.minimum
+end
+
+"Compute the minimum of the target function
+while one of the parameters is frozen"
+function minimize_frozen(
+    idx::I,
+    value::F,
+    θ₀::Vector{F},
+)::Float64
+    # Exclude the frozen parameter from the active ones
+    θ₀ = [θ₀[1:idx-1]; θ₀[idx+1:end]]
+    # Recreate the target function, handling a frozen parameter
+    function target_inner(θ::Vector{F})
+        return target([θ[1:idx-1]; value; θ[idx:end]])
+    end
+    # Optimize the new target function
+    res = optimize(target_inner, θ₀)
+    # Return the minimum
+    return res.minimum
+end
+
+"Compute the confidence intervals for the parameter"
+function intervals(idx::I, value::F, L₀::F)::Tuple{F,F,F,F}
+    # Create an alias function for the frozen parameter
+    minimize_with(p) = minimize_frozen(idx, p, θ₀)
+    # Find the first root
+    r = find_zero((p) -> minimize_with(p) - L₀ - 0.5, value)
+    # Find the second one nearby; compute the confidence intervals
+    r_left = r_right = σ_left = σ_right = 0
+    # If the root is to the left from the value
+    if value - r > 0
+        r_left = r
+        σ_left = abs(value - r_left)
+        r_right = find_zero((p) -> minimize_with(p) - L₀ - 0.5, value + σ_left)
+        σ_right = abs(value - r_right)
+    else
+        r_right = r
+        σ_right = abs(value - r_right)
+        r_left = find_zero((p) -> minimize_with(p) - L₀ - 0.5, value - σ_right)
+        σ_left = abs(value - r_left)
+    end
+    return r_left, r_right, σ_left, σ_right
+end
+
+"Plot the profile of the parameter"
+function plot_profile(
+    idx::I,
+    value::F,
+    name::Symbol,
+    r_left::F,
+    r_right::F,
+    σ_left::F,
+    σ_right::F,
+    L₀::F,
+)
+    # Create an alias function for the frozen parameter
+    minimize_with(p) = minimize_frozen(idx, p, θ₀)
+    # Plot the profile of the parameter
+    p = plot(
+        minimize_with,
+        r_left - σ_left,
+        r_right + σ_right;
+        xlabel=latexstring("$(name)"),
+        ylabel=latexstring("L_$(name)($(name))")
+    )
+    # Add vertical lines to the plot
+    plot!(p, [r_left, r_left], [L₀ - 0.2, L₀ + 0.5]; linestyle=:dash)
+    plot!(p, [value, value], [L₀ - 0.2, L₀]; linestyle=:dash)
+    plot!(p, [r_right, r_right], [L₀ - 0.2, L₀ + 0.5]; linestyle=:dash)
+    # Add points to the plot
+    scatter!(p, [r_left], [L₀ + 0.5,])
+    scatter!(p, [value], [L₀,])
+    scatter!(p, [r_right], [L₀ + 0.5,])
+    # Add horizontal lines to the plot
+    hline!(p, [L₀ + 0.5,]; linestyle=:dash)
+    hline!(p, [L₀,]; linestyle=:dash)
+    # Add annotations to the plot
+    annotate!(
+        p,
+        [
+            (v + 0.2 * (value - r_left), L₀ - 0.1, text("$(round(v; digits=2))", 8))
+            for v in [r_left, value, r_right]
+        ]
+    )
+    annotate!(
+        p,
+        [
+            (value - 1.85 * (value - r_left), L₀ + 0.06, text(L"L_0", 8)),
+            (value - 1.65 * (value - r_left), L₀ + 0.5 + 0.06, text(L"L_0 + 1/2", 8)),
+        ]
+    )
+
+    # Save the figure
+    savefig(p, "$(PLOTS_DIR)/$(name).pdf")
+end
+
+println(pad, "> Fitting the linear model by numerical optimization...")
+
+# Fit the linear model
+a_n, b_n, ∑_n = minimize()
+
+println('\n', pad, pad, "a: ", a_n)
+println(pad, pad, "b: ", b_n)
+println(pad, pad, "∑: ", ∑_n, '\n')
+
+println(pad, "> Computing the confidence intervals...")
+
+# Compute the confidence intervals
+a_left, a_right, a_σ_left, a_σ_right = intervals(1, a_n, ∑_n)
+b_left, b_right, b_σ_left, b_σ_right = intervals(2, b_n, ∑_n)
+
+println('\n', pad, pad, "a_left: ", a_left)
+println(pad, pad, "a_right: ", a_right)
+println(pad, pad, "σ_left: ", a_σ_left)
+println(pad, pad, "σ_right: ", a_σ_right, '\n')
+println(pad, pad, "b_left: ", b_left)
+println(pad, pad, "b_right: ", b_right)
+println(pad, pad, "σ_left: ", b_σ_left)
+println(pad, pad, "σ_right: ", b_σ_right, '\n')
+
+println(pad, "> Plotting the profiles...")
+
+# Plot the profiles
+plot_profile(1, a_n, :a, a_left, a_right, a_σ_left, a_σ_right, ∑_n)
+plot_profile(2, b_n, :b, b_left, b_right, b_σ_left, b_σ_right, ∑_n)
 
 println()
